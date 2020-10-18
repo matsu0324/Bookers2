17aa2e177e1ca1b27d07c8dfb1d4efa1bd20f66e
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-10-18 06:51:25 +0000

    [Start] フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定します。
+  has_many :followed_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # @user.booksのように、@user.yyyで、
+  # そのユーザがフォローしている人orフォローされている人の一覧を出したい
+  has_many :followeds, through: :followed_relationships, source: :zzz
   attachment :profile_image
 
   def already_favorited?(book)
