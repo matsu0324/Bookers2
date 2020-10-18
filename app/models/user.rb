5c576ac938a55eb002f3ee4854637ea5866fc951
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-10-18 14:41:37 +0000

    [Finish] フォロー＆フォロワー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  attachment :profile_image
+
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定します。
-  has_many :followed_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  # @user.booksのように、@user.yyyで、
-  # そのユーザがフォローしている人orフォローされている人の一覧を出したい
-  has_many :followeds, through: :followed_relationships, source: :zzz
-  attachment :profile_image
+
+  has_many :reverse_of_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 
   def already_favorited?(book)
     self.favorites.exists?(book_id: book.id)
