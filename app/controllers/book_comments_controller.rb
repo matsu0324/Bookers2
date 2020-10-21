64e679250eaa2e9c93b5bb13762db6a1f8988c4b
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-10-21 08:16:52 +0000

    [Finish] 非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,16 +2,24 @@ class BookCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    @book = Book.find(params[:book_id])
+    comment = @book.book_comments.new(book_comment_params)
+    comment.book_id = @book.id
+    comment.user_id = current_user.id
     comment.save
-    redirect_to book_path(book)
+    # unless comment.save
+    #   render 'error'
+    # end
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book_comment = BookComment.find(params[:id])
+    @book = @book_comment.book
+    @book_comment.destroy
+    # @book = Book.find(params[:book_id])
+    # book_comment = @book.book_comments.find(params[:id])
+    # book_comment.destroy
+    # BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
   end
 
 
