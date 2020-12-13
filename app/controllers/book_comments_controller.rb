1cbf31c9eabd5285ecb195b93e27be3ce722df99
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-12-13 16:19:04 +0000

    [Fix] インデント

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -7,21 +7,13 @@ class BookCommentsController < ApplicationController
     comment.book_id = @book.id
     comment.user_id = current_user.id
     comment.save
-    # unless comment.save
-    #   render 'error'
-    # end
   end
 
   def destroy
     @book_comment = BookComment.find(params[:id])
     @book = @book_comment.book
     @book_comment.destroy
-    # @book = Book.find(params[:book_id])
-    # book_comment = @book.book_comments.find(params[:id])
-    # book_comment.destroy
-    # BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
   end
-
 
 private
  def book_comment_params
